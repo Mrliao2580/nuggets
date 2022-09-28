6c3de33f419801ac03af547cbd18321d724732aa
xiaoliao <2973075721@qq.com>
2022-09-28 18:23:10 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
+/*
+ * @Author: lzh
+ * @Date: 2022-09-21 18:24:46
+ * @LastEditors: lzh
+ * @LastEditTime: 2022-09-28 18:21:52
+ * @Description: file content
+ */
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    iconPaths: {
+      favicon32: 'favicon.ico',
+      favicon16: 'favicon.ico',
+      appleTouchIcon: 'favicon.ico',
+      maskIcon: 'favicon.ico',
+      msTileImage: 'favicon.ico'
+    }
+  }
 })
