7548639fd5caa2a3bf2f0528f8429d59ecbc3c6e
xiaoliao <2973075721@qq.com>
2022-09-28 17:21:13 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,36 @@
  * @Author: lzh
  * @Date: 2022-09-21 18:24:46
  * @LastEditors: lzh
- * @LastEditTime: 2022-09-28 16:45:19
+ * @LastEditTime: 2022-09-28 17:20:47
  * @Description: app
 -->
 <template>
   <div id="app">
+    <div id="changeLang">
+      <div @click="changLang('zh')" class="chinese">中文</div>
+      <div class="long-string">丨</div>
+      <div @click="changLang('en')" class="english">English</div>
+    </div>
     <router-view />
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      Meg: 'Hello Word!',
+    };
+  },
+  methods: {
+    changLang(type) {
+      // 此处做了语言选择记录
+      localStorage.setItem('locale', type);
+      // 修改显示语言
+      this.$i18n.locale = type;
+    },
+  },
+};
+</script>
 
 <style lang="less">
 #app {
@@ -17,5 +39,37 @@
     padding: 0;
     margin: 0;
   }
+}
+
+#changeLang {
+  height: auto;
+  position: fixed;
+  top: 40px;
+  right: 40px;
+  z-index: 999;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  // 设置文字不能被选中
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  .chinese {
+    padding: 2px 6px 2px 8px;
+    cursor: pointer;
+    &:hover {
+      background-color: rgb(217, 216, 216);
+    }
+  }
+  .english {
+    cursor: pointer;
+    padding: 2px 8px 2px 6px;
+    &:hover {
+      background-color: rgb(217, 216, 216);
+    }
+  }
+  border: 1px solid #9e9e99;
+  border-radius: 4px;
 }
 </style>
--- src/views/demo/demo1.vue
+++ src/views/demo/demo1.vue
@@ -2,12 +2,13 @@
  * @Author: lzh
  * @Date: 2022-09-28 16:42:56
  * @LastEditors: lzh
- * @LastEditTime: 2022-09-28 16:52:17
+ * @LastEditTime: 2022-09-28 17:14:30
  * @Description: demo1
 -->
 <template>
   <div>
-    <h2>demo1</h2>
+    <h2>{{$t('name')}}</h2>
+    <h2>{{$t('info.age')}}</h2>
   </div>
 </template>
 
